2624f413e169898428b43e973737c8647856e9ac
Shayan Umair <144595179+Shayan1453@users.noreply.github.com>
2025-02-16 18:04:36 +0000

    conversion of datatype into anyother

---
+++ Practise1/conversion-operation.js
@@ -0,0 +1,13 @@
+// let score ="50ajfb"
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let IsLoggedIn = 1
+// let booleanIsLoggedIn= Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let anyNumber = 39
+let stringanyNumber = String(anyNumber)
+console.log(typeof stringanyNumber)
