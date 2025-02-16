e15f2b2dc9f3609cf914ac6f0e0ef339a8ec8f27
Shayan Umair <144595179+Shayan1453@users.noreply.github.com>
2025-02-16 09:46:39 +0000

    Variable learning

---
+++ Practise1/variable.js
@@ -0,0 +1,6 @@
+const accountId = "Shayan9689"
+let accountEmail = "shayan.umairadditise@gmail.com" 
+let accountPassword = "968946us"
+accountCity = "Lahore"
+let accountState = "Punjab" ;
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState])
