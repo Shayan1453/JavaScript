5455eeebff6be28d1d38e9f417ee6f1ee604030e
Shayan Umair <144595179+Shayan1453@users.noreply.github.com>
2025-02-19 16:19:04 +0000

    usage of strings and its functionality

---
+++ Practise1/strings.js
@@ -0,0 +1,22 @@
+// const name = "Shayan Umair"
+// const age = "18"
+// const hobby ="Watch movies"
+
+// console.log(`Hi my name is ${name} & my age is ${age}. My hobby is to ${hobby} `);
+
+// const Company = new String( "Telocom")
+// console.log(Company.length);
+// console.log(Company.__proto__);
+// console.log(Company.toUpperCase());
+// console.log(Company.charAt(6));
+// console.log(Company.indexOf('o'));
+
+// const Email = ( '      1238y@gmail.com     ');
+// console.log(Email);
+
+// console.log(Email.trim());
+
+// const url = ('https://chatgpt.com/c/67b5ff05shayan2ac69eafaf8e')
+// console.log(url.replace("shayan","ali" ));
+
+// console.log(url.includes(".com"));
