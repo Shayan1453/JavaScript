b153dee01c27ac01954ff8321a83d74f7c6ca192
Shayan Umair <144595179+Shayan1453@users.noreply.github.com>
2025-02-19 19:38:30 +0000

    How to use mathematical logics and numbers cases.

---
+++ Practise1/num.math.js
@@ -0,0 +1,29 @@
+// const socre = 120
+// console.log(socre);
+
+// const Balance = new Number(1200)
+// console.log(Balance);
+// console.log(Balance.toString());
+// console.log(Balance.toFixed(2));
+// console.log(Balance.toLocaleString());
+
+//**************************  MATHS  ******************************** */
+// console.log(Math);
+// console.log(Math.abs(-746));
+// console.log(Math.ceil(16.17));
+// console.log(Math.floor(16.17));
+// console.log(Math.round(14.68));
+// console.log(Math.min(16.17,16.5,7,56.3));
+// console.log(Math.max(16.17,16.5,7,56.3));
+
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 1
+const max =100
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
