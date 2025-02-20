f72bc74a4f06301451a4c0d07d1e8a0b555cc62e
Shayan Umair <144595179+Shayan1453@users.noreply.github.com>
2025-02-20 09:22:50 +0000

    Arrays structures and their properties

---
+++ 02_Basics/array.js
@@ -0,0 +1,16 @@
+//////   Array     //////
+const myArr = [1, 2, 3, 4]
+const myBooks = ["Urdu", "English", "Maths", "Science"]
+const myArr2 = new Array(9, 8, 7, 6, 5)
+
+console.log(myArr2[2]);
+
+/////     Array Methods    //////
+
+myBooks.push("Urdu2")
+myBooks.pop()
+console.log(myBooks);
+
+
+
+
