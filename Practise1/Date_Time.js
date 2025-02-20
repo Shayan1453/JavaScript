b731993ac7fc1a56fc7e96e6dac6ba6c8c969a60
Shayan Umair <144595179+Shayan1453@users.noreply.github.com>
2025-02-20 08:46:36 +0000

    Date & time query in js

---
+++ Practise1/Date_Time.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString);
+console.log(myDate.toDateString);
+console.log(myDate.toJSON);
+console.log(typeof myDate);
+
+
+// let myNewDate = new Date (2025, 1, 20)
+// let myNewDate = new Date (2025, 1, 20, 0, 38)
+let myNewDate = new Date ("2025-02,20")
+console.log(myNewDate.toJSON());
+
